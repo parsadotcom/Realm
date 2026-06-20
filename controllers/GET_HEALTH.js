6ce84b45e4e1e52f2735924514710df7727a8a90
parsadotcom <parsa.yorol@gmail.com>
2026-06-20 23:16:48 +0330

    perf: upgrading controllers flexibility, error handling, responses and format

--- controllers/GET_HEALTH.js
+++ controllers/GET_HEALTH.js
@@ -1,5 +1,12 @@
+import { successfulResponse, failedResponse } from "../responses/responses.js";
+
 /// API_ROUTE
 export const GET_HEALTH = (req, res) => {
-  console.log("Request for /health");
-  res.json({ status: "Realm is alive! 🛡️", statusCode: 200 });
+  try {
+    console.log("Request for /health");
+    res.status(200).json(successfulResponse("Realm is alive🛡️"));
+  } catch (healthError) {
+    console.log(healthError.message);
+    res.status(400).json(failedResponse("Request is not healthy", { name: healthError.name, reason: healthError.message }));
+  }
 }
--- controllers/POST_SIGNIN.js
+++ controllers/POST_SIGNIN.js
@@ -1,32 +1,28 @@
 import { signin_schema } from "../model/SignIn.model.js";
 import { GetUser } from "../config/db.js";
 import bcrypt from "bcryptjs";
+import { failedResponse, successfulResponse } from "../responses/responses.js";
+import { createToken } from "../tokens/jwtToken.js";
+
 
 export const POST_SIGNIN = async (req, res) => {
   console.log("User is trying to signin");
   try {
     const user = await GetUser(req.body);
     if (verifyPassword(req.body.password, user.password)) {
-      res.status(200).json({
-        success: true,
-        message: "You are successfuly signed in"
-      });
+      res.status(200).json(successfulResponse("You successfully signed into your account.", {
+        token: createToken(req.body.username, req.body.email)
+      }));
     } else {
-      res.status(400).json({
-        success: false,
-        message: "Password or email is wrong!",
-      })
+      res.status(400).json(failedResponse("Wrong Password", { type: "authentication" }));
     }
   } catch (signInError) {
-    res.status(400).json({
-      success: false,
-      error: signInError,
-    })
+    res.status(400).json(failedResponse("Unsuccessful signin", { name: signInError.name, reason: signInError.message }));
     console.log(`Error[ ${signInError}]`);
   }
 }
 
 async function verifyPassword(pass, hash) {
-  const result = await bcrypt.compare(pass, hash); 
+  const result = await bcrypt.compare(pass, hash);
   return result;
 }
