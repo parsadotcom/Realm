c34ded9e060f790c9e37846c7b2af51174918f80
parsadotcom <parsa.yorol@gmail.com>
2026-06-21 20:06:34 +0330

    feat: adding email verification to prevent bots This feature will prevent attackers from operating on their accounts

--- router.js
+++ router.js
@@ -5,6 +5,7 @@ import { m_userValidation } from './middlewares/validation/User.validation.js';
 import { m_hashPassword } from './middlewares/m_passwordHashMiddleware.js';
 import { POST_SIGNIN } from './controllers/POST_SIGNIN.js';
 import { m_signInValidation } from './middlewares/validation/User.signin.validation.js';
+import { POST_VERIFYEMAIL } from "./controllers/POST_VERIFYEMAIL.js";
 // check the options of router!
 
 export const router = new Router();
@@ -12,3 +13,4 @@ export const router = new Router();
 router.get("/health", GET_HEALTH);
 router.post("/users/api/v1/signup", m_userValidation, m_hashPassword, POST_SIGNUP);
 router.post("/users/api/v1/signin", m_signInValidation, POST_SIGNIN); /// UPGRADE: here!
+router.post("user/api/v1/verifyemail", POST_VERIFYEMAIL);
